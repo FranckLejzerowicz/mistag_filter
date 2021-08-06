7ec89a96ac1c3b79d3c6c4e5d3aa81251175b763
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2021-08-05 22:39:54 -0700

    allow outputting fastqs

--- mistag_getter.py
+++ mistag_getter.py
@@ -3,31 +3,36 @@ import argparse
 
 from mistag_utils import *
 
+
 def get_args():
-    parser=argparse.ArgumentParser()
-    parser.add_argument('-i', nargs = 1, required = True, help = 'Input fasta \
-                        file name [or folder if pre-formatting needed] \
-                        (required)')
-    parser.add_argument('-d', nargs = 1, required = True, help = 'Multiplexing \
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', nargs=1, required=True, help='Input fasta \
+                        file name [or library folder containing the \
+                        `_fwd.fastq` and `_rev.fastq` files and the \
+                        `_mistag_R1.fastq` and `_mistag_R2.fastq` files] \
+                        (required))')
+    parser.add_argument('-d', nargs=1, required=True, help='Multiplexing \
                         design file name (required)')
-    parser.add_argument('-o', nargs = '?', default = argparse.SUPPRESS, help =
-                        "Output fasta file (default = input appended with \
+    parser.add_argument('-o', nargs='?', default=argparse.SUPPRESS, help =
+                        "Output fasta file (default=input appended with \
                         'mistagFiltered.fasta')")
-    parser.add_argument('-a', nargs = '?', default = 0.05, type = float,
+    parser.add_argument('-a', nargs='?', default=0.05, type=float,
                         metavar='float between 0 and 1, max. 3 decimals',\
-                        choices = [float(x)/1000 for x in range(1,1001)], help =
+                        choices=[float(x)/1000 for x in range(1,1001)], help =
                         "Alpha level for finding the Student's T critical value \
                         for the modified Thompson Tau rejection region \
-                        calulation (default = 0.05)")
-    parser.add_argument('--out', action = 'store_true', default = False, help =
+                        calulation (default=0.05)")
+    parser.add_argument('--out', action='store_true', default=False, help =
                         "Leave expected sample sequences out of non-critical \
                         mistags distribution for calculations of the rejection \
-                        region (default = not active)")
-    parser.add_argument('-m', nargs = '?', choices = ['pandaseq', 'vsearch'],
-                        default = 'pandaseq', help = 'Reads merging software - \
-                        which must be installed and running (default = pandaseq)')
-    parse=parser.parse_args()
-    args=vars(parse)
+                        region (default=not active)")
+    parser.add_argument('-m', nargs='?', choices=['pandaseq', 'vsearch'],
+                        default='pandaseq', help='Reads merging software - \
+                        which must be installed and running (default=pandaseq)')
+    parser.add_argument('--fastq', action='store_true', default=False,
+                        help="Output per-sample fastq files")
+    parse = parser.parse_args()
+    args = vars(parse)
     return args
 
 
@@ -37,7 +42,7 @@ def get_fastqs(fastin, design):
     """
     samples_fastqs = {}
     # get the fwd/rev fasta files per sample
-    files =  glob.glob('%s/*fastq' % fastin)
+    files = glob.glob('%s/*fastq' % fastin)
     for f in sorted(files):
         fsplit = f.split('_')
         if fsplit[-1] in ['rev.fastq', 'fwd.fastq']:
@@ -46,15 +51,23 @@ def get_fastqs(fastin, design):
     design_rev = rev_dict(design)
     # add the sample name for each fwd/rev fastqs list
     add_to_dict_list(samples_fastqs, design_rev)
-    return samples_fastqs
+
+    combi_fastqs = {}
+    for _, (fwd, rev, cmb) in samples_fastqs.items():
+        if cmb in combi_fastqs:
+            print('%s affected twice...' % cmb)
+            sys.exit()
+        combi_fastqs[cmb] = [fwd, rev]
+
+    return samples_fastqs, combi_fastqs
 
 
-def get_mistags_files(fastin, primers_rad):
-    fs =  glob.glob('%s/*fastq' % fastin)
+def get_mistags_files(fastin):
+    # get unexpected samples information
+    fs = glob.glob('%s/*fastq' % fastin)
     mistag_fastqs = [f for f in fs if re.search(r'_mistag_R[1-2]\.fastq$', f)]
     if len(mistag_fastqs) == 2:
         return mistag_fastqs
-        # get unexpected samples information
     print('No pair of mistag files detected.\nExiting...')
     sys.exit()
 
@@ -62,24 +75,23 @@ def get_mistags_files(fastin, primers_rad):
 def get_output_fp(args):
     f = args['i'][0]
     if 'o' in args:
-        oFas = args['o']
-        oRad = '%s_mistagFilt' % os.path.splitext(oFasta)[0]
+        o_fas = args['o']
+        o_rad = '%s_mistagFilt' % os.path.splitext(o_fas)[0]
     else:
         curdir = os.path.split(os.path.abspath(f))
         if os.path.isdir(f):
-            oRad = '%s/%s_mistagFilt' % (os.path.abspath(f), curdir[1])
+            o_rad = '%s/%s_mistagFilt' % (os.path.abspath(f), curdir[1])
         else:
-            oRad = '%s_mistagFilt' % os.path.splitext(os.path.abspath(f))[0]
-        oFas = '%s.fasta' % oRad
-    return oRad, oFas
+            o_rad = '%s_mistagFilt' % os.path.splitext(os.path.abspath(f))[0]
+        o_fas='%s.fasta' % o_rad
+    return o_rad, o_fas
 
 
 def get_saturation(design, primers):
-    n_F = len(primers['F'])
-    n_R = len(primers['R'])
-    FR = float(n_F * n_R)
-    n_S = float(len(design))
-    sat_frac = (n_S / (FR))
-    sat_perc = round((sat_frac * 100), 2)
-    return FR, sat_perc
-
+    n_f = len(primers['F'])
+    n_r = len(primers['R'])
+    fr = float(n_f * n_r)
+    n_s = float(len(design))
+    sat_fraction = (n_s / fr) * 100
+    sat_percent = round(sat_fraction, 2)
+    return fr, sat_percent
